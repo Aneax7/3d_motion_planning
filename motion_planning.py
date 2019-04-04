ad8c8907ee3ad657d51ab52eae18459fc6c6fc35
Aneax7 <acharyanish7@gmail.com>
2019-04-04 23:42:31 +0545

    comments

--- motion_planning.py
+++ motion_planning.py
@@ -169,8 +169,7 @@ class MotionPlanning(Drone):
 
 
         # Run A* to find a path from start to goal
-        # TODO: add diagonal motions with a cost of sqrt(2) to your A* implementation
-        # or move to a different search space such as a graph (not done here)
+  
         print('Local Start and Goal: ', grid_start, grid_goal)
         path, _ = a_star(grid, heuristic, grid_start, grid_goal)
         
